869aecc8ca5da614bbbf0434fcbdd9911638a898
ankkhaha <66871130+ankkhaha@users.noreply.github.com>
2020-06-16 19:57:54 +0700

    Update 008.php

--- 008.php
+++ 008.php
@@ -7,12 +7,26 @@ class Student {
     // gán giá trị mặc định cho thuộc tính $name
     public $name = 'demo họ tên';
 
+    // thêm thuộc tính tĩnh
+    public static $location = 'việt nam';
+
 
     // phương thức tĩnh
     public static function demo1() {
         echo '<br>' . __METHOD__;
         // không được sử dụng từ khóa $this trong phương thức tĩnh
         // nếu sử dụng thì sẽ bị lỗi
+        echo '<br> truy cập đến phương thức thông thường từ bên trong phương thức tĩnh';
+        $self = new self();
+        // truy cập thuộc tính bình thường
+        echo '<br>' . $self->name;
+        // truy cập phương thức bình thức
+        echo '<br>' . $self->demo2();
+
+        // truy cập đến thuộc tính tĩnh
+        echo '<br> truy cập đến thuộc tính tĩnh';
+        echo '<br>' . self::$location;
+
     }
 
     // phương thức bình thường
